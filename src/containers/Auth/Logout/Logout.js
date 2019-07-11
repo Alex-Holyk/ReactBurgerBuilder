348699cdc3d3cbcd071a1b26a0618a7b2c6f384e
Alex Holyk <o.holyk@apiko.com>
2019-07-11 18:57:35 +0300

    up to 442

--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -5,7 +5,6 @@ import { connect } from "react-redux";
 import * as actions from "../../../store/actions/";
 
 const Logout = props => {
-  //
   const { onLogout } = props;
   useEffect(() => {
     onLogout();
